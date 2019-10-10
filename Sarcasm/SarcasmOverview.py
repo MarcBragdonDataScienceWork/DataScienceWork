f80e5147b380c5ad4d940b4143ff5b63dd3ced13
Marc Bragdon <mabragdo@syr.edu>
2019-10-09 21:32:23 -0400

    Added overviews

---
+++ Sarcasm/SarcasmOverview.py
@@ -0,0 +1,5 @@
+The sarcasm project requires all of the dependencies denoted in the requirements.txt file. 
+
+The analysis_final.py file contains all the working code for the xgboost, linearregression and neural network approaches to the analysis. 
+
+The data file is very large and wasn't included but could be furnished if needed. 
